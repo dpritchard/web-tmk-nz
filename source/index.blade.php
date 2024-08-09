f13c83f816ff2231b441fbe8b427bf01419188e8
Daniel Pritchard <daniel@pritchard.co>
2024-08-09 13:01:59 +1200

    Setup

---
+++ source/index.blade.php
@@ -0,0 +1,8 @@
+@extends('_layouts.main')
+
+@section('body')
+<div class="p-8">
+    <h1 class="text-3xl font-bold">TMK Research Limited</h1>
+    <p class="font-mono">Site Under Construction.</p>
+</div>
+@endsection
