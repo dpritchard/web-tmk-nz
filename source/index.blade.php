8645f0c5c4d4244b66ddab22eaf1678701623506
Daniel Pritchard <daniel@pritchard.co>
2025-06-17 12:58:16 +1200

    Update w redirect

--- source/index.blade.php
+++ source/index.blade.php
@@ -1,8 +1,5 @@
-@extends('_layouts.main')
-
-@section('body')
-<div class="p-8">
-    <h1 class="text-3xl font-bold">TMK Research Limited</h1>
-    <p class="font-mono">Site Under Construction.</p>
-</div>
-@endsection
+<!DOCTYPE html>
+<meta charset="utf-8">
+<title>Redirecting to the Te Tiaki Mahinga Kai Partnership</title>
+<meta http-equiv="refresh" content="0; URL=https://www.mahingakai.org.nz">
+<link rel="canonical" href="https://www.mahingakai.org.nz">
